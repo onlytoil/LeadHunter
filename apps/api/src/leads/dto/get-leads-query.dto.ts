ab1c6353cf681c2497cefb5ad99fd594125cbf92
Albert <anadyrov2009@gmail.com>
2026-08-04 20:30:08 +0500

    feat: add lead search filters

--- apps/api/src/leads/dto/get-leads-query.dto.ts
+++ apps/api/src/leads/dto/get-leads-query.dto.ts
@@ -1,8 +1,25 @@
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+
 import { LeadStatus } from '../../generated/prisma/client';
 
 export class GetLeadsQueryDto {
   @IsOptional()
   @IsEnum(LeadStatus)
   status?: LeadStatus;
+
+  @IsOptional()
+  @IsString()
+  chat?: string;
+
+  @IsOptional()
+  @IsString()
+  keyword?: string;
+
+  @IsOptional()
+  @IsDateString()
+  dateFrom?: string;
+
+  @IsOptional()
+  @IsDateString()
+  dateTo?: string;
 }
